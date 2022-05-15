6709018f29f76f3bcefce0ab6c85f48940fc4568
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-05-16 01:19:37 +0900

    Add: wordlist 데이터 가져오기 완료

--- src/WordList.tsx
+++ src/WordList.tsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 interface Word {
   text: string
@@ -26,11 +28,22 @@ function WordList() {
   // warning!
   // 만약 어떠한 이유로 작동이 되지 않는다면, 그 문제를 우회해서
   // 전체 기능이 동작하도록 코드를 구현.
-  const wordlist: Word[] = [
-    { text: 'apple', meaning: 'n. 사과' },
-    { text: 'brick', meaning: 'n. 벽돌' },
-    { text: 'leap', meaning: 'v. 뛰다, 급증하다' }
-  ]
+
+  const [wordlist, setWordlist] = useState<Array<Word>>([])
+
+  // const wordlist: Word[] = [
+  //   { text: 'apple', meaning: 'n. 사과' },
+  //   { text: 'brick', meaning: 'n. 벽돌' },
+  //   { text: 'leap', meaning: 'v. 뛰다, 급증하다' }
+  // ]
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://joseph-proxy.herokuapp.com/https://solution-tmp.s3.ap-northeast-2.amazonaws.com/vocabs.json'
+      )
+      .then((res) => setWordlist(res.data))
+  }, [])
 
   return (
     <section>
